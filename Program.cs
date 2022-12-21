ce354eb8b50c643c4f7a2c4829c16c952be2c5f8
Lucas Silva <nascimentolucas786@gmail.com>
2022-12-21 12:10:55 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("123456", "modelo 1", "111111", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Iphone iphone = new Iphone("654321", "modelo 2", "222222", 512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
